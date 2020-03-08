53e9bbfcf11680229c5caba82957a5a53b677f13
Kicend <kicend@gmail.com>
2020-03-08 19:37:05 +0100

    Naprawa błędu związanego z kończeniem czyszczenia bez skutku

--- data/cleanup.py
+++ data/cleanup.py
@@ -11,7 +11,10 @@ def delete_unused_dirs():
         if today_day > date_day or today_month > date_month or today_year > date_year:
             directory_list.insert(directory_list.index(date), today)
             break
-    start_index = directory_list.index(today) + 1
-    while start_index != len(directory_list):
-        rmtree("class_files/{}".format(directory_list[start_index]))
-        start_index += 1
+    try:
+        start_index = directory_list.index(today) + 1
+        while start_index != len(directory_list):
+            rmtree("class_files/{}".format(directory_list[start_index]))
+            start_index += 1
+    except ValueError:
+        pass
