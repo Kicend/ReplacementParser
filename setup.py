1e14a06e0b292c055d1e12008724a39d70725d39
Kicend <kicend@gmail.com>
2020-04-06 18:34:32 +0200

    Instalator

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ReplacementParser',
+    version='1.0.0',
+    packages=['beautifulsoup4', 'lxml', 'requests', 'certifi'],
+    url='https://github.com/Kicend/ReplacementParser',
+    license='MIT',
+    author='Filip "Kicend" Szczepanowski',
+    author_email='kicend@gmail.com',
+    description='Parser do strony http://zastepstwa.staff.edu.pl/'
+)
